228593cfa42658f4cb0e59edb21effed4886ec2f
JusCampin <37031311+JusCampin@users.noreply.github.com>
2022-09-20 19:30:34 -0700

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "adamant"
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game "rdr3"
+
+description 'Rexshack Gaming : Max Cores'
+
+client_scripts {
+	'client/client.lua',
+}
+
+lua54 'yes'
